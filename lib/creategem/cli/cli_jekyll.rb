9b4f4daac15984455ed432247c9c744bfe0f644a
Mike Slinn <mslinn@mslinn.com>
2023-06-09 14:30:13 -0400

    -

--- lib/creategem/cli/cli_jekyll.rb
+++ lib/creategem/cli/cli_jekyll.rb
@@ -13,50 +13,48 @@ module Creategem
       Publish the gem on a private repository.
     END_DESC
 
-    method_option :type, type: :string, default: 'tag', enum: %w[tag block generator], repeatable: true,
+    method_option :type, type: :string, default: 'tag', enum: %w[tag block generator], repeatable: false,
       desc: 'Specifies the types of plugin.'
 
     method_option :host, type: :string, default: 'github', enum: %w[bitbucket github],
       desc: 'Repository host.'
 
     def jekyll(gem_name)
+      @gem_name = gem_name
       @dir = Creategem.dest_root gem_name
       @jekyll = true
-      @jekyll_type = options[:type] || ['tag']
-      create_gem_scaffold gem_name
-      create_jekyll_scaffold gem_name
+      @jekyll_types = [options[:type] || 'tag']
 
-      case @jekyll_type
-      when 'tag' || ''
-        create_jekyll_tag_scaffold gem_name
-      when 'block'
-        create_jekyll_block_scaffold gem_name
-      when 'generator'
-        create_jekyll_generator_scaffold gem_name
-      else
-        abort "Error: Invalid jekyll --type: #{@jekyll_type}"
-      end
+      create_gem_scaffold gem_name
+      create_jekyll_scaffold
+
+      create_jekyll_tag_scaffold       if @jekyll_types.include? 'tag'
+      create_jekyll_block_scaffold     if @jekyll_types.include? 'block'
+      create_jekyll_generator_scaffold if @jekyll_types.include? 'generator'
 
       initialize_repository gem_name
     end
 
     private
 
-    def create_jekyll_scaffold(_gem_name)
-      say "Creating a new Jekyll scaffold for a new gem named #{gem_name} in #{@dir}", :green
+    def create_jekyll_scaffold
+      say "Creating a new Jekyll scaffold for a new gem named #{@gem_name} in #{@dir}", :green
       directory 'jekyll_scaffold', @dir
     end
 
-    def create_jekyll_tag_scaffold(gem_name)
-      # TODO: write me
+    def create_jekyll_tag_scaffold
+      say 'Creating a new Jekyll tag scaffold', :green
+      directory 'jekyll_tag_scaffold', @dir
     end
 
-    def create_jekyll_block_scaffold(gem_name)
-      # TODO: write me
+    def create_jekyll_block_scaffold
+      say 'Creating a new Jekyll tag block scaffold', :green
+      directory 'jekyll_block_scaffold', @dir
     end
 
-    def create_jekyll_generator_scaffold(gem_name)
-      # TODO: write me
+    def create_jekyll_generator_scaffold
+      say 'Creating a new Jekyll generator scaffold', :green
+      directory 'jekyll_generator_scaffold', @dir
     end
   end
 end
