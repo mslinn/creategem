b909bac2ee87d5ac059e839ab8e086fa4b8a7782
Mike Slinn <mslinn@mslinn.com>
2023-06-25 11:29:34 -0400

    -

--- lib/creategem/cli/cli_rails.rb
+++ lib/creategem/cli/cli_rails.rb
@@ -5,10 +5,10 @@ module Creategem
     include Thor::Actions
     include Creategem::Git
 
-    desc 'plugin NAME', 'Creates a new Rails plugin scaffold.'
+    desc 'rails NAME', 'Creates a new Rails rails scaffold.'
 
     long_desc <<~END_DESC
-      Creates a new Rails plugin scaffold with the given NAME,
+      Creates a new Rails rails scaffold with the given NAME,
       by default hosted by GitHub and published on RubyGems.
     END_DESC
 
@@ -26,7 +26,7 @@ module Creategem
 
     private_option
 
-    def plugin(gem_name)
+    def rails(gem_name)
       @host           = options['host']
       @mountable      = options['mountable']
       @private        = options['private']
@@ -34,10 +34,10 @@ module Creategem
 
       @dir = Creategem.dest_root gem_name
       @engine = @host || @mountable
-      @plugin = true
+      @rails = true
 
       create_gem_scaffold gem_name
-      create_plugin_scaffold gem_name
+      create_rails_scaffold gem_name
       create_engine_scaffold gem_name if @engine
       create_mountable_scaffold gem_name if @mountable
       initialize_repository gem_name
@@ -55,11 +55,11 @@ module Creategem
       directory 'rails/mountable_scaffold', @dir, force: true, mode: :preserve
     end
 
-    def create_plugin_scaffold(gem_name)
-      say "Creating a new Rails plugin scaffold for gem named #{gem_name} in #{@dir}", :green
-      directory 'rails/plugin_scaffold', @dir, force: true, mode: :preserve
+    def create_rails_scaffold(gem_name)
+      say "Creating a new Rails plugin scaffold as a gem named #{gem_name} in #{@dir}", :green
+      directory 'rails/rails_scaffold', @dir, force: true, mode: :preserve
       Dir.chdir @dir do
-        run 'chmod +x test/dummy/bin/*'
+        run 'chmod +x test/dummy/bin/*' # TODO: fix me
       end
     end
   end
