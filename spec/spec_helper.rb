8fa72be8e2d3d66be68fd4462d2019bb52149655
Mike Slinn <mslinn@mslinn.com>
2023-11-09 13:31:21 -0500

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
 require_relative '../lib/nugem'
 
 RSpec.configure do |config|
-  config.filter_run :focus
   # config.order = "random"
-  config.run_all_when_everything_filtered = true
 
   # See https://relishapp.com/rspec/rspec-core/docs/command-line/only-failures
   config.example_status_persistence_file_path = '../spec/status_persistence.txt'
